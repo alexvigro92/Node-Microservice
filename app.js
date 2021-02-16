d56578f770cbf593b8dcce8798cdf995910d3587
Alejandro Villa <alejandro_villa@192.168.1.11>
2021-02-16 12:07:39 -0600

    added a env variable

--- app.js
+++ app.js
@@ -4,18 +4,13 @@ const router = require('./src/routes')
 const path = require('path');
 const { json, urlencoded } = express
 const app = express()
-const port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+const host = process.env.IP  || '0.0.0.0'
+const port = process.env.PORT || 8080
 app.use(json())
 app.use(urlencoded({ extended: false }))
 const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
 app.use(cors(corsOptions))
 app.use(router)
-app.get('/health-check',(req,res)=> {
-res.send("Health check passed")
-})
-app.get('/bad-health',(req,res)=> {
-res.status(500).send("Health check did not pass")
-})
 app.use('/home',(req,res) => { res.sendFile(path.join(__dirname+'/src/html/index.html')); })
 app.use('/', (req, res) => { res.send("this is the API v1.0.0"); })
-app.listen(port, () => { console.log(`Server listening on port ${port}`); })
+app.listen(port,host, () => { console.log(`Server listening on port ${port} in the host ${host}`); })
