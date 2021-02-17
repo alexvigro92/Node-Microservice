866b4becb10efda9fd41ca858a181553b91261dd
Alejandro Villa <alexvigro92@gmail.com>
2021-02-17 12:27:07 -0600

    added return ip to req

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
 app.use(cors(corsOptions))
 app.use(router)
 app.use('/home',(req,res) => { res.sendFile(path.join(__dirname+'/src/html/index.html')); })
-app.use('/', (req, res) => { res.send("this is the API v1.0.0"); })
+app.use('/', (req, res) => { res.send("this is the API v1.0.0 ${req.connection.remoteAddress} ${req.ip}");})
 app.listen(port,host, () => { console.log(`Server listening on port ${port} in the host ${host}`); })
